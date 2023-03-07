dbe8c4b2274e3aa3a7957888063ca5da0632ac5d
Paul John <rainson.work@gmail.com>
2023-03-07 15:20:38 +0300

    variables and names

--- 0x01-python_hardway/0x00-drills/0-drill_ex1.py
+++
@@ -1,6 +0,0 @@
-print("This is the line")
-print('I can print more of this')
-print("I just need enough time to type them out")
-# this is a commnent.
-# we use an octothorpe or hash for comments
-# print("this won't be printed to the console")
