a7ee0cd5b0b12003659129b20a2a92005acb6187
Paul John <rainson.work@gmail.com>
2023-03-07 15:59:15 +0300

    added comments

--- 0x01-python_hardway/5-ex6.py
+++ 0x01-python_hardway/5-ex6.py
@@ -10,15 +10,21 @@ y = f"Those who know {binary} and those who {do_not}."
 print(x)
 print(y)
 
+# f.string inserts the value of x and y
+# at the place holders{x} and {y} respectively
 print(f"I said: {x}")
 print(f"I also said: '{y}'")
 
+# assign values to variables
 hilarious = False
 joke_evaluation = "Isn't that joke so funnny?! {}"
 
+# .format() inserts the value of hilarious
+# into the space '{}' in the joke_evaluation string
 print(joke_evaluation.format(hilarious))
 
 w = "This is the left side of ..."
 e = "a string with a right side."
 
+# prints the values of w and e respectively
 print(w + e)
