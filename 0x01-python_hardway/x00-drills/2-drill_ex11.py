c7798d647c7e9a311eaa55430c3c664db49066f9
Paul John <rainson.work@gmail.com>
2023-03-07 21:24:36 +0300

    taking input from user

---
+++ 0x01-python_hardway/x00-drills/2-drill_ex11.py
@@ -0,0 +1,9 @@
+print("Hi! I am your assistant,", end=' ')
+name = input("What's your name? ")
+
+print("How are you feeling {}".format(name))
+tmp = input()
+
+print("I see you're feeling", tmp, "\n")
+print(f"It was nice talking to you {name}!")
+print("I hope to talk to you again soon : )")
