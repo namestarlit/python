8aefed28e2ecf764418e0422ded7dd1c2171f98e
Paul John <rainson.work@gmail.com>
2023-03-09 12:31:06 +0300

    while loop and list

---
+++ 0x01-python_hardway/33-ex33.py
@@ -0,0 +1,19 @@
+# initialize i to 0
+i = 0
+numbers = []
+
+# while-loop to append values into numbers list
+while i < 6:
+    print(f"At the top i is {i}")
+    numbers.append(i)
+
+    # update i, increment by 1
+    i = i + 1
+    print("Numebrs now: ", numbers)
+    print(f"At the bottom i is {i}")
+
+print("The numbers: ")
+
+# for loop to print numbers
+for num in numbers:
+    print(num)
