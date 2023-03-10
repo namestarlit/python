4228890b2b8088321067191826f1710fe184964a
Paul John <rainson.work@gmail.com>
2023-03-10 10:28:33 +0300

    class in python

---
+++ 0x01-python_hardway/MyStuff.py
@@ -0,0 +1,8 @@
+# this is a class
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
