1aadd7c6cca47f563340849fd1609279f3556858
Paul John <rainson.work@gmail.com>
2023-03-10 10:54:28 +0300

    added my favorite song

--- 0x01-python_hardway/40-ex40.py
+++ 0x01-python_hardway/40-ex40.py
@@ -13,6 +13,17 @@ happy_bday = Song(["Happy birthday to you",
 
 bulls_on_parade = Song(["They rally around tha family",
                         "With pockets full of shells"])
+author = "Jesca Pratt"
+
+song = [f"""
+This time around has it gone
+so gray that my fear can't hold out.
+\tsong by {author}
+"""]
+
+this_time_around = Song(song)
+
+this_time_around.sing_me_a_song()
 
 happy_bday.sing_me_a_song()
 
