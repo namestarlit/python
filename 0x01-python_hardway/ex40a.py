33434546941c75206740848ce01686ff87aaddfb
Paul John <rainson.work@gmail.com>
2023-03-10 10:28:49 +0300

    added a class

--- 0x01-python_hardway/ex40a.py
+++ 0x01-python_hardway/ex40a.py
@@ -1,7 +1,29 @@
-# import module
+# import modules
 import mystuff
+
+# this is a class
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
 
 # access a function apple() from mystuff
 mystuff.apple()
 # print a variable tangerine from mystuff
 print(mystuff.tangerine)
+
+# getting things from 'things'
+
+# class style
+
+# instantiate a class
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
+
+
+# dict style
+#MyStuff['apples']
