c5e94a143f6abbe20d2e32da755ccfba18862b42
Paul John <rainson.work@gmail.com>
2023-08-09 17:23:31 +0300

    Modify init file to import configuration

--- 0x03-Flask/microblog/models/__init__.py
+++ 0x03-Flask/microblog/models/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 # Import routes here to avoid circular imports.
 from models import routes
