af86c54d6944855b3fa5d9229f97ae178d7016e1
Paul John <rainson.work@gmail.com>
2023-08-12 00:22:02 +0300

    Add `forms.py`

---
+++ 0x03-Flask/microblog/models/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    """A LoginForm class definition."""
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
