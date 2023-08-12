8dd4c19783a9ca99f67ca54668eefc1f2fafeabf
Paul John <rainson.work@gmail.com>
2023-08-12 11:06:01 +0300

    Update `config.py`

--- 0x03-Flask/microblog/config.py
+++ 0x03-Flask/microblog/config.py
@@ -1,7 +1,15 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     """A Flask configuration class."""
     # Get secret key otherwise assign a default value.
     SECRET_KEY = os.environ.get('SECRET_KEY', 'you-will-never-guess')
+
+    # SQLAlchemy configuration.
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///'
+                                             + os.path.join(basedir, 'app.db')
+                                             )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
