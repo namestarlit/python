6858e1477ae722331831e222608ef6ab3440ab78
Paul John <rainson.work@gmail.com>
2023-08-12 12:43:38 +0300

    Update `models.py`

--- 0x03-Flask/microblog/microblog/models.py
+++ 0x03-Flask/microblog/microblog/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from microblog import db
 
 
@@ -8,6 +10,7 @@ class User(db.Model):
                          nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         """String representantion of User class."""
@@ -15,4 +18,21 @@ class User(db.Model):
                 .format(self.__class__.__name__,
                         self.id, self.username,
                         self.email)
+                )
+
+
+class Post(db.Model):
+    """Defines a class Post."""
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        """String representantion of Post class."""
+        return ("<{}: (id={}, timestamp='{}', user_id={})>"
+                .format(self.__class__.__name__,
+                        self.id,
+                        self.timestamp,
+                        self.user_id)
                 )
