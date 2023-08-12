9e4c21225aa1f1e13f7d1d342f7e9448eef0cbbb
Paul John <rainson.work@gmail.com>
2023-08-12 15:36:46 +0300

    Modify `app.py`

--- 0x03-Flask/microblog/app.py
+++ 0x03-Flask/microblog/app.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 """Main application module."""
 
-from microblog import app
+from microblog import app, db
+from microblog.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """Pre-imports the application instances."""
+    instances = {
+            'db': db,
+            'User': User,
+            'Post': Post
+            }
+
+    return instances
 
 
 if __name__ == '__main__':
