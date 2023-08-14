f0fc07017a71bb94f5c646e26fbc1503fae34f5e
Paul John <rainson.work@gmail.com>
2023-08-14 11:45:29 +0300

    Add user description and last seen field

--- 0x03-Flask/microblog/microblog/models.py
+++ 0x03-Flask/microblog/microblog/models.py
@@ -20,6 +20,8 @@ class User(UserMixin, db.Model):
                          nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
