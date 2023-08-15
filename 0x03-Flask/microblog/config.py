19cc06143d1ba7debbf73aa57982e6a820111a34
Paul John <rainson.work@gmail.com>
2023-08-15 22:28:17 +0300

    Add Logging error by email

--- 0x03-Flask/microblog/config.py
+++ 0x03-Flask/microblog/config.py
@@ -13,3 +13,12 @@ class Config(object):
                                              + os.path.join(basedir, 'app.db')
                                              )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+    # Email Server configuration
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 25))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
--- 0x03-Flask/microblog/microblog/__init__.py
+++ 0x03-Flask/microblog/microblog/__init__.py
@@ -1,3 +1,6 @@
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -26,3 +29,27 @@ from microblog import routes, models, errors
 # Create tables within an application context
 #with app.app_context():
 #    db.create_all()
+
+# Log errors by email.
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'],
+                    app.congig['MAIL_PASSWORD']
+                    )
+            secure = None
+
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                    mailhost=(
+                        app.config['MAIL_SERVER'],
+                        app.config['MAIL_PORT']),
+                    fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                    toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                    credentials=auth, secure=secure
+                    )
+            mail_handler.setLevel(loggin.ERROR)
+            app.logger.addHandler(mail_handler)
