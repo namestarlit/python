be5da3ff406c5a66a154f12ae5a2d12cbcf56118
Paul John <rainson.work@gmail.com>
2023-08-17 16:26:41 +0300

    Modify database sql scripts

--- 0x03-Flask/microblog/setup/setup_mysql_dev.sql
+++ 0x03-Flask/microblog/setup/setup_mysql_dev.sql
@@ -2,7 +2,7 @@
 -- Creates a development database
 CREATE DATABASE IF NOT EXISTS app_dev_db;
 
-CREATE USER IF NOT EXISTS 'app_dev'@'localhost' IDENTIFIED BY 'app_dev_pwd';
+CREATE USER IF NOT EXISTS 'app_dev'@'localhost' IDENTIFIED BY 'App_dev_v0.1';
 GRANT ALL PRIVILEGES ON app_dev_db . * TO 'app_dev'@'localhost';
 GRANT SELECT ON performance_schema . * TO 'app_dev'@'localhost';
 FLUSH PRIVILEGES;
--- 0x03-Flask/microblog/setup/setup_mysql_test.sql
+++ 0x03-Flask/microblog/setup/setup_mysql_test.sql
@@ -2,7 +2,7 @@
 -- Create a test database
 CREATE DATABASE IF NOT EXISTS app_test_db;
 
-CREATE USER IF NOT EXISTS 'app_test'@'localhost' IDENTIFIED BY 'app_test_pwd';
+CREATE USER IF NOT EXISTS 'app_test'@'localhost' IDENTIFIED BY 'App_test_v0.1';
 GRANT ALL PRIVILEGES ON app_test_db . * TO 'app_test'@'localhost';
 GRANT SELECT ON performance_schema . * TO 'app_test'@'localhost';
 FLUSH PRIVILEGES;
